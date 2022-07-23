3df5b61dfd8d12d3fa9e1c72e62e171e805fec32
sanz17 <kundusanhita13@gmail.com>
2022-07-23 11:54:57 +0530

    ft: auto-close tab

--- extension/background.js
+++ extension/background.js
@@ -7,13 +7,35 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       iconUrl: "icons/icon_128.png",
     };
 
+    // async function deleteCurrentTab() {
+    //   let queryOptions = { active: true, lastFocusedWindow: true };
+    //   let [tab] = await chrome.tabs.query(queryOptions);
+    //   tabId=parseInt(tab)
+    //   chrome.tabs.remove(tabId)
+    // }
+    // deleteCurrentTab();
+
+    
+    
     if (request.id === "feedback") {
       chrome.notifications.create(request.id, options, (id) => {
         console.log(`notification sent - ${id}`);
+        chrome.tabs.query({
+          currentWindow:true,
+          active:true
+        },function(tabs){
+          chrome.tabs.remove(tabs[0].id);
+        });
       });
     } else {
       chrome.notifications.create(options, (id) => {
         console.log(`notification sent - ${id}`);
+        chrome.tabs.query({
+          currentWindow:true,
+          active:true
+        },function(tabs){
+          chrome.tabs.remove(tabs[0].id);
+        });
       });
     }
   }
